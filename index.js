f3d6044211d86ef9d2d72f98197ddbda0c55986d
Chris Wallace <Chrisw1096@gmail.com>
2022-07-21 12:54:56 +1000

    fleshed out classes in lib file, added switch statements to input types and insert content html file based on title, when looped though

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
+//const Employee = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const employeeArr = [];
 
 const promptUserManager = () => {
+    console.log("please enter the team managers details:");
     return  inquirer.prompt([  
     {
         type: 'input',
@@ -11,7 +17,7 @@ const promptUserManager = () => {
     {
         type: 'input',
         name: 'id',
-        message: 'Id:',
+        message: 'Id: (interger)',
     },
     {
         type: 'input',
@@ -20,127 +26,112 @@ const promptUserManager = () => {
     },
     {
         type: 'input',
-        name: 'github',
-        message: 'Github:',
+        name: 'officeNum',
+        message: 'Office Number:',
     },]
-    )};
+    )
 
-// class Employee {
-//     constructor(name, id, email) {
-//       this.name = name;
-//       this.id = id;
-//       this.email= email;
-//     }
-
-// getName(){
-// $(this.name)
-// }
-
-// getId(){
-// $(this.id)
-// }
-
-// getEmail(){
-// $(this.email)
-// }
-
-// getRole(){
-
-// }
-
-// }
-
-// `&mdash;returns 'Employee'`
-
-// class Manager extends Employee {
-//     }
+    .then(({name, id, email, officeNum}) => {
+        const manager = new Manager(name, id, email, officeNum)
+        employeeArr.push(manager);
+        console.log(employeeArr)
+        promptUserRole();
+    })
+};
 
 
 const promptUserRole = () => {
-     inquirer.prompt([  
+     return inquirer.prompt([  
     {
-        type: 'checkbox',
+        type: 'list',
         message: 'Role?',
         name: 'role',
-        choices: ['Engineer', 'Intern'],
+        choices: ['Engineer', 'Intern', 'Finish'],
     },]
-    )
-    if(role == 'Engineer'){
-        return  inquirer.prompt([   
-            {
-            type: 'input',
-            name: 'name',
-            message: 'Name:',
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Id:',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Email:',
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Github:',
-        },]
-        )
-    }
-    if (role == 'Intern'){
-        return  inquirer.prompt([  
-            {
-            type: 'input',
-            name: 'name',
-            message: 'Name:',
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Id:',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Email:',
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: 'School Name:',
-        },]
-        ) 
-    }
+    ).then((choice) => {
+        switch (choice.role){
+            case 'Engineer':
+                createEngineer();
+                break;
+            case 'Intern':
+                 createIntern();
+                  break;
+            case 'Finish':
+                  buildTeam()
+                break;
+            default:
+                  buildTeam()
+        }
+    })
 };
 
-const genHTML=({name, id, email, github, role})=>
-    `
-    <!doctype html>
+const buildTeam = () => {
+fs.writeFile('HTML_response.html', genHTML(employeeArr, (err) => {
+    if (err) throw err;
+    console.log("build succeeded")
+}) )
+}
+
+
+const genHTML = (data) => {
+return `
+<!doctype html>
 <html>
 <head>
 <title>HTML Generator</title>
 <meta name="description" content="HTML Gen">
 </head>
 <body>
-${name}
-${id}
-${email}
-${github}
-${role}
+    
+${data.map((employee) => {
+    console.log(employee)
+    if(employee.getRole() == 'Manager' ){
+        return `
+        hello there
+        `
+    } else if (employee.getRole() == 'Intern' ){
+        return `
+        Intern
+        `
+    }else if (employee.getRole() == 'Engineer' ){
+        return `
+        Engineer
+        `
+    }
+})}
+    
 </body>
 </html>
-    `;
+    
+`
+
+}
+
+//return 'Employee'
+//     `
+//     <!doctype html>
+// <html>
+// <head>
+// <title>HTML Generator</title>
+// <meta name="description" content="HTML Gen">
+// </head>
+// <body>
+// getName()
+// ${id}
+// ${email}
+// ${github}
+// ${role}
+// </body>
+// </html>
+//     `;
 
 
 const init = () => {
 promptUserManager()
-.then(promptUserRole())
 
-    .then((responses) => fs.writeFileSync('HTML_response.html', genHTML(responses)))
-    .then(() => console.log('Successfully wrote to HTML_response.html'))
-    .catch((err) => console.error(err));
+    // .then((responses) => fs.writeFileSync('HTML_response.html', genHTML(responses)))
+    // .then(() => console.log('Successfully wrote to HTML_response.html'))
+    // .catch((err) => console.error(err));
 };
 
 init();
